803eadbdf5bba71043569e7e641f5bef6f5f7bdc
clodioneto <clodio.alves@gmail.com>
2021-10-27 09:11:35 -0300

    Criando CRUD

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
+
+
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +12,29 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
-
+  constructor(private activatedRoute: ActivatedRoute, private apiservice: ApiService) {
+    //this.readData();
+    this.creatData();
+   }
+    
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    // this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+    creatData() {
+      const data: any = {
+        title: 'Clodio',
+        body: 'Teste',
+        userId: 10
+      }
+      this.apiservice.createData(data).subscribe(data => {
+        console.log(data);
+      })
+    }
+
+  readData() {
+    this.apiservice.readData().subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
